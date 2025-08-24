cbda5fe41fb81645bcce336eb3164e98e89822d5
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-08-24 19:10:07 +0300

    Step 3

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
