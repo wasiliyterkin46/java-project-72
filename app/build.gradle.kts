a872d06e6f9415843a6462dd1d49ddd3ff63b20d
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-08-24 12:10:31 +0300

    Step 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "6.2.0.5505"
+    id("io.freefair.lombok") version "8.13.1"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+checkstyle {
+    toolVersion = "11.0.0"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "wasiliyterkin46_java-project-78")
+        property("sonar.organization", "wasiliyterkin46")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
